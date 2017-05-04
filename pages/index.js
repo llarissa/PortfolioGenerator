37989538a44bbe420db9dc65af5d57832b8b7776
Larissa <larissa-schoellhorn@gmx.de>
2017-05-04 10:24:20 +0200

    spezifischer Link

--- pages/index.js
+++ pages/index.js
@@ -109,7 +109,7 @@ export default class Index extends Component {
           <ul>
             {
               messages &&
-              Object.keys(messages).map(key => <li key={key}><Link href="/portfolio"><a>{messages[key].text}</a></Link></li>)
+              Object.keys(messages).map(key => <li key={key}><Link href={"/portfolio" + "#"+ messages[key].text}><a>{messages[key].text}</a></Link></li>)
             }
           </ul>
         </div>
