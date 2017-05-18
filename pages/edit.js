0b61ecc5e2d9c2791ad05eada5e8328f6af4603a
Robert Kim <ichrobby@gmail.com>
2017-05-18 16:57:50 +0200

    Edit aufgeräunt und Firebase Datenbank reader hinzhugefügt

--- pages/edit.js
+++ pages/edit.js
@@ -10,26 +10,79 @@ import Save from '../pages/saveChanges'
 
 
 export default class Edit extends Component {
+  static async getInitialProps ({req, query}) {
+    const user = req && req.session ? req.session.decodedToken : null    
+    let inspList = null;
+    if(req && req.firebaseServer)
+    {          
+    const snap = await req.firebaseServer.database().ref('messages').once('value') //db änderung handler
+    inspList = snap.val();
+    }
+    return { user, messages: inspList }
+  }
 
-constructor() {
-    super()
+constructor(probs) {
+    super(probs)
     this.state = {
       Überschrift: null,
       Inhalt: null,
-      Unterschrift: null
+      Unterschrift: null,
+      messages: this.props.messages
     }
+    this.addDbListener = this.addDbListener.bind(this)   
   }
   
-  
+   componentDidMount () {
+    if(firebase.apps.length === 0)
+    {
+      firebase.initializeApp(clientCredentials)
+    }
+
+    if (this.state.user) this.addDbListener()    
+
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        this.setState({ user: user })
+        return user.getToken()
+          .then((token) => {
+            // eslint-disable-next-line no-undef
+            return fetch('/api/login', {
+              method: 'POST',
+              // eslint-disable-next-line no-undef
+              headers: new Headers({ 'Content-Type': 'application/json' }),
+              credentials: 'same-origin',
+              body: JSON.stringify({ token })
+            })
+          }).then((res) => this.addDbListener())
+      } else {
+        this.setState({ user: null })
+        // eslint-disable-next-line no-undef
+        fetch('/api/logout', {
+          method: 'POST',
+          credentials: 'same-origin'
+        }).then(() => firebase.database().ref('messages').off())
+      }
+    })
+  }
+
+  addDbListener () {    
+    firebase.database().ref('messages').on('value', snap => {
+      const messages = snap.val()
+      if (messages) this.setState({ messages })
+    })
+  }
 
  render () {
+  const { messages } = this.state
 
     return <Layout>
       <Save>
       </Save>
       
          <div className = "title"> 
-             <h1>Hier sollte der Name des Portfolioinhabers stehen</h1>
+             <h1 key={this.props.url.query.id}>
+                    {messages[this.props.url.query.id].text}                 
+                </h1>
          </div>
     
           <ImageUploading >
