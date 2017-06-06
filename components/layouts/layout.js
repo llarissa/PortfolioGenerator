085b95fba13c8e6a1b0e5d454e5716f22705327b
Larissa <larissa-schoellhorn@gmx.de>
2017-06-06 19:57:43 +0200

    button style  + nav bar

--- components/layouts/layout.js
+++ components/layouts/layout.js
@@ -11,10 +11,7 @@ export default ({ children, title = 'Der Portfolio Generator' }) => (
     </Head>
     <header>
       <nav>
-        <Link href='/'><a>Home</a></Link> |
-        <Link href='/about'><a>About</a></Link> |
-        <Link href='/contact'><a>Contact</a></Link> 
-        
+        <Link href='/'><a>Home</a></Link>         
       </nav>
     </header>
 
